f8c9a4cd6dad8e2af657329bd917724f90aa6d92
pravinkumararbad <pravinkumar.arbad@gmail.com>
2025-05-30 00:30:44 +0530

    Update ArrayChallenges.cpp

--- ArrayADT/ArrayADT/ArrayChallenges.cpp
+++ ArrayADT/ArrayADT/ArrayChallenges.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
-// 1. Find a single missing element from a sorted array.
-// 2. Find multiple missing elements from a sorted array.
-// 3. Find missing elements in unsorted array.
+// 1. Find a single missing element from a sorted array. (The sorted arrays are first n natural numbers.)
+
+// 2. Find a single missing element from a sorted array. (The sorted arrays are arbitarary n natural numbers.)
+
+// 3. Find multiple missing elements from a sorted array.
+
+// 4. Find missing elements in unsorted array.
